f68861387d3637ab318901088ac54e08c01711cc
JonathanPerlman <hp0533165306@gmail.com>
2024-09-08 12:36:41 +0300

    Ex

--- ES6/script.js
+++ ES6/script.js
@@ -1,45 +1,50 @@
-//Ex 1
-function printArrow(lines, direction) {
-    let arrowBody = '-'.repeat(lines);
-    if (direction) {
-        console.log(arrowBody + '>');
-    } else {
-        console.log('<' + arrowBody);
-    }
-}
+// //Ex 1
+// function printArrow(lines, direction) {
+//     let arrowBody = '-'.repeat(lines);
+//     if (direction) {
+//         console.log(arrowBody + '>');
+//     } else {
+//         console.log('<' + arrowBody);
+//     }
+// }
 
-// printArrow(5, true);  
-// printArrow(3, false); 
+// // printArrow(5, true);  
+// // printArrow(3, false); 
 
-//Ex 2
+// //Ex 2
 
-let numbers = [];
-let input;
+// let numbers = [];
+// let input;
 
-while (true) {
-    input = parseInt(prompt("Plese enter a number: "), 10);
+// while (true) {
+//     input = parseInt(prompt("Plese enter a number: "), 10);
     
-    if (input === 0) {
-        break;
-    }
+//     if (input === 0) {
+//         break;
+//     }
     
-    numbers.push(input);
-}
+//     numbers.push(input);
+// }
 
-if (numbers.length > 0) {
-    let maxNumber = Math.max(...numbers);
-    let sum = numbers.reduce((acc, num) => acc + num, 0);
-    let average = sum / numbers.length;
-    let fourthNumber = numbers.length >= 4 ? numbers[3] : numbers[numbers.length - 1];
+// if (numbers.length > 0) {
+//     let maxNumber = Math.max(...numbers);
+//     let sum = numbers.reduce((acc, num) => acc + num, 0);
+//     let average = sum / numbers.length;
+//     let fourthNumber = numbers.length >= 4 ? numbers[3] : numbers[numbers.length - 1];
 
-    console.log(maxNumber);
-    console.log(numbers.length);
-    console.log(sum);
-    console.log(average);
-    console.log(fourthNumber);
-} else {
-    console.log("לא הוזנו מספרים.");
-}
+//     console.log(maxNumber);
+//     console.log(numbers.length);
+//     console.log(sum);
+//     console.log(average);
+//     console.log(fourthNumber);
+// } else {
+//     console.log("לא הוזנו מספרים.");
+// }
 
 //Ex3
-
+function printTriangle(num) {
+    for (let i = 1; i <= num; i++) {
+        console.log('*'.repeat(i));
+    }
+}
+console.log( printTriangle(5));
